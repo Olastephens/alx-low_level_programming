34bc49453bc9aa8f0d908544d4b8bb68009bf50a
Olasunkanmi <hollarmi20@gmail.com>
2023-03-16 21:56:53 -0700

    Print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	pritf("with proper grammar, but the outcome is a piece of art,\");
-		return(0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
+
